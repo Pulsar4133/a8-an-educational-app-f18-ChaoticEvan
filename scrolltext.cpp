67dc6939bc094dc5abf94b00f922838850da8111
Theauxm <theaux.masquelier@gmail.com>
2019-11-24 16:27:03 -0700

    News scroll text working

--- scrolltext.cpp
+++ scrolltext.cpp
@@ -1,6 +1,88 @@
 #include "scrolltext.h"
+#include <QPainter>
+#include <QWidget>
+#include <QHBoxLayout>
 
-ScrollText::ScrollText()
+ScrollText::ScrollText(QWidget *parent) : QWidget(parent), scrollPos(0)
+{
+    // Sets variables for actual text
+    staticText.setTextFormat(Qt::PlainText);
+    setFixedHeight(fontMetrics().height() + 10);
+    leftMargin = width();
+    seperator = "   ";
+    timer.setInterval(30);
+
+    QObject::connect(&timer, &QTimer::timeout, this, &ScrollText::timer_timeout);
+}
+
+ScrollText::~ScrollText()
 {
 
 }
+
+QString ScrollText::getText()
+{
+    return text;
+}
+
+void ScrollText::setText(QString _text)
+{
+    text = _text;
+    updateText();
+    update();
+}
+
+QString ScrollText::getSeperator()
+{
+    return seperator;
+}
+
+void ScrollText::setSeperator(QString _seperator)
+{
+    seperator = _seperator;
+    updateText();
+    update();
+}
+
+void ScrollText::updateText()
+{
+    singleTextWidth = fontMetrics().width(text);
+    timer.start();
+    scrollPos = 0;
+    staticText.setText(seperator + text);
+    staticText.prepare(QTransform(), font());
+    wholeTextSize = QSize(fontMetrics().width(staticText.text()), fontMetrics().height());
+}
+
+void ScrollText::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+    buffer = QImage(size(), QImage::Format_ARGB32_Premultiplied);
+
+    buffer.fill(qRgba(0, 0 ,0, 0));
+    QPainter pb(&buffer);
+    pb.setPen(painter.pen());
+    pb.setFont(painter.font());
+
+    int x = qMin(-scrollPos, 0)+ leftMargin + (leftMargin/2);
+
+    if(x < width())
+    {
+        pb.drawStaticText(QPointF(x, (height() - wholeTextSize.height()) / 2) + QPoint(0,0), staticText);
+        x +=  wholeTextSize.width();
+
+        if(x < 0)
+        {
+            scrollPos = 0;
+        }
+
+        painter.drawImage(0, 0, buffer);
+    }
+}
+
+void ScrollText::timer_timeout()
+{
+    scrollPos = (scrollPos + 2);
+    update();
+}
+
